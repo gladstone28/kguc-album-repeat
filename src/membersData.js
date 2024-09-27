42d090fd8ac70d8e13e1af39a9724e349c7c4820
Gladstone Rose <gladstonerose@hotmail.com>
2024-09-27 13:04:12 -0500

    modify MemberData.js, added gladstone.jpg and renamed file

--- src/membersData.js
+++ src/membersData.js
@@ -10,7 +10,14 @@ const members = [
     name: 'Jane Smith',
     profilePicture: '/images/jane.jpg',
     description: 'Sunday School teacher and coordinator of community outreach programs.'
+  },
+ {
+    id: 3,
+    name: 'Gladstone Rose',
+    profilePicture: '/images/gladstone.jpg',
+    description: 'Secretary for the Planning Committee and senior member of Daniel House and member of the Technical Team.'
   }
+
 ];
 
 export default members;
