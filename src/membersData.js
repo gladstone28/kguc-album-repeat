443540758004cc8fd021da6e7a93ef41c3bdd05c
Gladstone Rose <gladstonerose@hotmail.com>
2024-09-27 19:48:40 -0500

    modify memnersData.js

--- src/membersData.js
+++ src/membersData.js
@@ -2,25 +2,28 @@ const members = [
   {
     id: 1,
     name: 'John Doe',
-    profilePicture: '/images/john.jpg',
+
+    profilePicture: process.env.PUBLIC_URL + '/images/john.jpg',
+
     description: 'Choir leader and head of the youth ministry.'
   },
   {
     id: 2,
     name: 'Jane Smith',
-    profilePicture: '/images/jane.jpg',
+
+    profilePicture: process.env.PUBLIC_URL + '/images/jane.jpg',
     description: 'Sunday School teacher and coordinator of community outreach programs.'
   },
  {
     id: 3,
     name: 'Gladstone Rose',
-    profilePicture: '/images/gladstone.jpg',
+    profilePicture: process.env.PUBLIC_URL + '/images/gladstone.jpg',
     description: 'Secretary for the Planning Committee and senior member of Daniel House and member of the Technical Team.'
   },
 {
     id: 4,
     name: 'Alaina Rose',
-    profilePicture: '/images/alaina.jpg',
+    profilePicture: process.env.PUBLIC_URL + '/images/alaina.jpg',
     description: 'Youth Fellowship Co-ordinator and member of the Technical Team.'
   }
 
