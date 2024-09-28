5119b6093251a842cfc1ca536c6389a4e6c9c89e
Gladstone Rose <gladstonerose@hotmail.com>
2024-09-27 20:27:55 -0500

    deleted file and modified file

--- src/membersData.js
+++ src/membersData.js
@@ -2,15 +2,12 @@ const members = [
   {
     id: 1,
     name: 'John Doe',
-
     profilePicture: process.env.PUBLIC_URL + '/images/john.jpg',
-
     description: 'Choir leader and head of the youth ministry.'
   },
   {
     id: 2,
     name: 'Jane Smith',
-
     profilePicture: process.env.PUBLIC_URL + '/images/jane.jpg',
     description: 'Sunday School teacher and coordinator of community outreach programs.'
   },
