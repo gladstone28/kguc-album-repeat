373bfb5b4306cfec22205beae8d06156e837e86f
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-01 04:08:52 -0500

    add new pics

--- src/backup-membersData.js
+++ src/backup-membersData.js
@@ -16,7 +16,7 @@ const members = [
     id: 3,
     name: 'Gladstone Rose',
     profilePicture: process.env.PUBLIC_URL + '/images/gladstone.jpg',
-    description: 'Secretary for the Planning Committee, senior member of Daniel House and member of the Technical Team.'
+    description: 'Secretary for the Planning Committee and senior member of Daniel House and member of the Technical Team.'
   },
 {
     id: 4,
@@ -59,42 +59,7 @@ const members = [
     name: 'Sample Three',
     profilePicture: process.env.PUBLIC_URL + '/images/sample3.jpg',
     description: 'Church: This is a sample - Career: Sample Profession.'
-  },
-{
-    id: 11,
-    name: 'Mikhail Reid',
-    profilePicture: process.env.PUBLIC_URL + '/images/mikhail.jpg',
-    description: 'Church: Praise Team Member - Career: Chemical Engineer.'
-  },
-{
-    id: 12,
-    name: 'Glodine Anderson',
-    profilePicture: process.env.PUBLIC_URL + '/images/glodine.jpg',
-    description: 'Church: Praise Team Member - Career: Pharmacist.'
-  },
-{
-    id: 13,
-    name: 'Jahmai Paul',
-    profilePicture: process.env.PUBLIC_URL + '/images/jahmai.jpg',
-    description: 'Church: Member of Praise team - Career: Entrepreneur.'
-  },
-{
-    id: 14,
-    name: 'Carleen Soares',
-    profilePicture: process.env.PUBLIC_URL + '/images/carleen.jpg',
-    description: 'Church: Usher, member of Daniel House - Career: Business Woman.'
-  },
-{
-    id: 15,
-    name: 'Garwell Bacchas',
-    profilePicture: process.env.PUBLIC_URL + '/images/garwell.jpg',
-    description: 'Church: Minister, Chairman of the Board - Career: Ordained Minister.'
   }
-
-
-
-
-
 
 
 
